f5e33004ae0afe41112b8fceea7a813ab56d3627
mafei <mafei.dev@gmail.com>
2022-02-12 16:59:34 +0530

    init project init

---
+++ starter-test-mysql/src/main/java/com/example/StarterTestMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterTestMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterTestMysqlApplication.class, args);
+    }
+
+}
