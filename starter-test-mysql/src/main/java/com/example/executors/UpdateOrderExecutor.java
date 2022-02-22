87124a4f8a55d173f96b8c5e22abc87f147f6360
mafei <mafei.dev@gmail.com>
2022-02-23 02:30:33 +0530

    getting reading to run using RevertExecutors

--- starter-test-mysql/src/main/java/com/example/executors/UpdateOrderExecutor.java
+++ starter-test-mysql/src/main/java/com/example/executors/UpdateOrderExecutor.java
@@ -3,11 +3,18 @@ package com.example.executors;
 import com.example.MyMicroServices;
 import com.example.aggregator.OrderAggregator;
 import org.mono.stacksaga.RevertHintStore;
+import org.mono.stacksaga.SagaAggregate;
 import org.mono.stacksaga.annotation.Executor;
+import org.mono.stacksaga.annotation.RevertAfter;
+import org.mono.stacksaga.annotation.RevertBefore;
+import org.mono.stacksaga.annotation.RevertExecutorConfig;
 import org.mono.stacksaga.exception.execution.NetworkException;
 import org.mono.stacksaga.exception.execution.ProcessException;
 import org.mono.stacksaga.exception.execution.RevertException;
 import org.mono.stacksaga.executor.CommandExecutor;
+import org.mono.stacksaga.executor.RevertExecutor;
+import org.mono.stacksaga.executor.SagaExecutor;
+import org.mono.stacksaga.executor.utils.ExecutorExecutedData;
 import org.mono.stacksaga.executor.utils.ProcessStack;
 import org.mono.stacksaga.executor.utils.ProcessStepManager;
 import org.mono.stacksaga.executor.utils.Wrapper;
@@ -21,7 +28,43 @@ public class UpdateOrderExecutor implements CommandExecutor<OrderAggregator> {
     }
 
     @Override
+    @RevertAfter(executors = {
+            @RevertExecutorConfig(order = 1, revertExecutor = Revert1.class),
+            @RevertExecutorConfig(order = 2, revertExecutor = Revert2.class),
+    })
+    @RevertBefore(executors = {
+            @RevertExecutorConfig(order = 0, revertExecutor = Revert3.class)
+    })
     public void doRevert(ProcessStack<OrderAggregator> finalProcessStack, ProcessException processException, RevertHintStore revertHintStore) throws RevertException {
         // TODO document why this method is empty
     }
+
+    @Executor(executeFor = "revert-service1")
+    public static class Revert1 implements RevertExecutor<OrderAggregator> {
+
+        @Override
+        public void doProcess(Map<Class<? extends SagaExecutor<? extends SagaAggregate>>, ExecutorExecutedData<OrderAggregator>> aggregateStateHistory, ProcessException processException) throws RevertException {
+
+        }
+    }
+
+    @Executor(executeFor = "revert-service1")
+    public static class Revert2 implements RevertExecutor<OrderAggregator> {
+
+        @Override
+        public void doProcess(Map<Class<? extends SagaExecutor<? extends SagaAggregate>>, ExecutorExecutedData<OrderAggregator>> aggregateStateHistory, ProcessException processException) throws RevertException {
+
+        }
+    }
+
+    @Executor(executeFor = "revert-service2")
+    public static class Revert3 implements RevertExecutor<OrderAggregator> {
+
+        @Override
+        public void doProcess(Map<Class<? extends SagaExecutor<? extends SagaAggregate>>, ExecutorExecutedData<OrderAggregator>> aggregateStateHistory, ProcessException processException) throws RevertException {
+
+        }
+    }
+
+
 }
