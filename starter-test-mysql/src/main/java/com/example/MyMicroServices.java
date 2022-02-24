a7564299a726ad2073fea22e98446feeea277a82
mafei <mafei.dev@gmail.com>
2022-02-25 05:24:09 +0530

    updated: treemap to NavigableMap interface

--- starter-test-mysql/src/main/java/com/example/MyMicroServices.java
+++ starter-test-mysql/src/main/java/com/example/MyMicroServices.java
@@ -4,4 +4,5 @@ public interface MyMicroServices {
     String ORDER_SERVICE = "order-service";
     String LOG_SERVICE = "log-service";
     String USER_SERVICE = "user-service";
+    String STOCK_SERVICE = "stock-service";
 }
--- starter-test-mysql/src/main/java/com/example/executors/CheckStockExecutor.java
+++ starter-test-mysql/src/main/java/com/example/executors/CheckStockExecutor.java
@@ -5,16 +5,17 @@ import com.example.aggregator.OrderAggregator;
 import org.mono.stacksaga.annotation.Executor;
 import org.mono.stacksaga.exception.execution.NetworkException;
 import org.mono.stacksaga.executor.QueryExecutor;
+import org.mono.stacksaga.executor.utils.AggregatorContainer;
 import org.mono.stacksaga.executor.utils.ProcessStack;
 import org.mono.stacksaga.executor.utils.ProcessStepManager;
-import org.mono.stacksaga.executor.utils.AggregatorContainer;
 
-@Executor(executeFor = MyMicroServices.USER_SERVICE)
-public class CheckUserExecutor implements QueryExecutor<OrderAggregator> {
+@Executor(executeFor = MyMicroServices.STOCK_SERVICE)
+public class CheckStockExecutor implements QueryExecutor<OrderAggregator> {
     @Override
     public ProcessStepManager<OrderAggregator> doProcess(ProcessStack<OrderAggregator> processStack, AggregatorContainer<OrderAggregator> aggregate) throws NetworkException {
-        aggregate.get().getTmp().setUsername("updated 1");
-        aggregate.get().setUsername("mafei updated");
-        return ProcessStepManager.next(CheckStockExecutor.class, aggregate);
+        OrderAggregator orderAggregator = aggregate.get();
+        orderAggregator.setUsername("updated 3");
+        orderAggregator.getTmp().setUsername("3ll3l3ll");
+        return ProcessStepManager.compete(aggregate);
     }
 }
