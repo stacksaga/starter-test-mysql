3f357024b856d695bd319af06cc82d8b0795cf37
mafei <mafei.dev@gmail.com>
2022-03-21 12:21:50 +0530

    added: common lot

---
+++ starter-test-mysql/src/test/java/com/example/controller/TransactionServiceImplTest.java
@@ -0,0 +1,55 @@
+package com.example.controller;
+
+
+import com.example.aggregator.OrderAggregator;
+import com.example.executors.ReserveOrder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mono.stacksaga.SagaTemplate;
+import org.mono.stacksaga.TransactionResponse;
+import org.mono.stacksaga.common.comiunication.TransactionTranceResponse;
+import org.mono.stacksaga.db.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TransactionServiceImplTest {
+    @Autowired
+    private TransactionService transactionService;
+    @Autowired
+    private SagaTemplate<OrderAggregator> orderAggregatorSagaTemplate;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+
+    @Test
+    void getTransactionTraceTest() {
+        OrderAggregator orderAggregator = new OrderAggregator();
+        orderAggregator.setUpdatedStatus("INIT_STEP>");
+        orderAggregator.setType(OrderAggregator.Type.revert_complete);
+        TransactionResponse<OrderAggregator> response = orderAggregatorSagaTemplate.doProcess(
+                orderAggregator,
+                ReserveOrder.class,
+                objectMapper
+        );
+
+        Optional<TransactionTranceResponse> transactionTrace = transactionService.getTransactionTrace(orderAggregator.getAggregateTransactionId());
+        if (!transactionTrace.isPresent()) {
+
+            Assertions.fail();
+        } else {
+            transactionTrace.get().getExecutorOrderAsDetailsMap().forEach(System.out::println);
+        }
+    }
+
+}
