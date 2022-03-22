b529a0bad8c9d3917ab3b63057635ec86647c96e
mafei <mafei.dev@gmail.com>
2022-03-23 02:13:01 +0530

    added: get hint stores

--- starter-test-mysql/src/main/java/com/example/aggregator/AddCustomerAggregator.java
+++ starter-test-mysql/src/main/java/com/example/aggregator/AddCustomerAggregator.java
@@ -1,9 +1,22 @@
 package com.example.aggregator;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
 import org.mono.stacksaga.SagaAggregate;
+import org.mono.stacksaga.core.AggregatorMapper;
+import org.mono.stacksaga.core.SagaAggregatorMapper;
 import org.mono.stacksaga.core.annotation.Aggregator;
 
-@Aggregator(version = "1.0.2")
+@Aggregator(version = "1.0.2", mapper = AddCustomerAggregator.Mapper.class)
 public class AddCustomerAggregator extends SagaAggregate {
+
+    @AllArgsConstructor
+    public static class Mapper implements AggregatorMapper {
+
+        @Override
+        public SagaAggregatorMapper setSagaAggregatorMapper() {
+            return SagaAggregatorMapper.Builder.build(new ObjectMapper());
+        }
+    }
 
 }
