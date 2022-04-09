74a732b220ab0db230423b4e3c569c5932637edb
mafei <mafei.dev@gmail.com>
2022-04-09 23:58:11 +0530

    fixed: after removing flat order

--- starter-test-mysql/src/main/java/com/example/executors/MakeThePaymentExecutor.java
+++ starter-test-mysql/src/main/java/com/example/executors/MakeThePaymentExecutor.java
@@ -7,6 +7,7 @@ import com.example.ms.customerwalletservice.repository.CustomerWalletRepository;
 import lombok.AllArgsConstructor;
 import org.mono.stacksaga.RevertHintStore;
 import org.mono.stacksaga.annotation.Executor;
+import org.mono.stacksaga.annotation.RevertAfter;
 import org.mono.stacksaga.annotation.RevertBefore;
 import org.mono.stacksaga.annotation.RevertExecutorConfig;
 import org.mono.stacksaga.exception.execution.ExecutorException;
@@ -34,10 +35,14 @@ public class MakeThePaymentExecutor implements CommandExecutor<OrderAggregator> 
     @RevertBefore(executors = {
             @RevertExecutorConfig(order = 1, revertExecutor = MakePaymentLogUpdate.class)
     })
+    @RevertAfter(executors = {
+            @RevertExecutorConfig(order = 1, revertExecutor = MakePaymentLogUpdate.class)
+    })
+
     public void doRevert(ProcessStack<OrderAggregator> previousProcessStack, ExecutorException executorException, OrderAggregator currentAggregate, RevertHintStore revertHintStore) throws IOException {
         System.out.println("MakeThePaymentExecutor.doRevert");
         revertHintStore.put("MakeThePaymentExecutor", new Date());
-      /*  if (currentAggregate.getType().equals(OrderAggregator.Type.revert_error)) {
+/*        if (currentAggregate.getType().equals(OrderAggregator.Type.revert_error)) {
             IOException ioException = new IOException("revert error from MakeThePaymentExecutor");
             ioException.printStackTrace();
             throw ioException;
