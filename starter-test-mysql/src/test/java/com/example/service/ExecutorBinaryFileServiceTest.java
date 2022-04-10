b527718cedb9e53907d5301c8fe690660c4e8518
mafei <mafei.dev@gmail.com>
2022-04-11 04:06:04 +0530

    added: send garbage to admin server [not finalized]

---
+++ starter-test-mysql/src/test/java/com/example/service/ExecutorBinaryFileServiceTest.java
@@ -0,0 +1,78 @@
+package com.example.service;
+
+import com.google.common.base.Stopwatch;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.SerializationUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mono.stacksaga.EnableStackSaga;
+import org.mono.stacksaga.common.comiunication.InstanceDetail;
+import org.mono.stacksaga.core.service.ExecutorBinaryFileService;
+import org.mono.stacksaga.common.comiunication.ExecutorExecutionType;
+import org.mono.stacksaga.redis.publisher.ExecutorBinaryTransformerService;
+import org.mono.stacksaga.redis.template.StringStringTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StopWatch;
+
+import java.io.File;
+import java.util.Date;
+import java.util.UUID;
+
+import static org.mono.stacksaga.common.Resources.Keys.ADMIN_SERVER_KEY_FORMATTER;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableStackSaga
+@Slf4j
+public class ExecutorBinaryFileServiceTest {
+
+    @Autowired
+    private ExecutorBinaryFileService executorFileSavingService;
+
+    @Autowired
+    private ExecutorBinaryTransformerService executorBinaryTransformerService;
+
+    @Autowired
+    private StringStringTemplate stringStringTemplate;
+
+    @Test
+    void saveAsFileAndSendToAdminServerTest() {
+        stringStringTemplate.getTemplate().opsForValue().set(String.format(ADMIN_SERVER_KEY_FORMATTER, "1"), "test");
+        InstanceDetail instanceDetail = new InstanceDetail();
+        instanceDetail.setStart_up_datetime(new Date().getTime());
+        String transactionUid = UUID.randomUUID().toString();
+        String executorUid = UUID.randomUUID().toString();
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("saveAsFile");
+        File file = executorFileSavingService.saveAsFile(
+                transactionUid,
+                executorUid,
+                ExecutorExecutionType.INIT_EXECUTION,
+                SerializationUtils.serialize(instanceDetail)
+        );
+        Assertions.assertNotNull(file);
+        stopWatch.stop();
+        log.info("AbsolutePath = {} ", file.getAbsolutePath());
+        log.info("Path = {} ", file.getPath());
+        log.info("Name = {} ", file.getName());
+        //send file
+        stopWatch.start("sendIndividualFile");
+        executorBinaryTransformerService.sendIndividualFile(file);
+        stopWatch.stop();
+        //delete after
+        stopWatch.start("deleteFile");
+        executorFileSavingService.deleteFile(file.getName());
+        stopWatch.stop();
+        for (StopWatch.TaskInfo taskInfo : stopWatch.getTaskInfo()) {
+            log.info("Time [ms] : {} : [{}]", taskInfo.getTimeMillis(), taskInfo.getTaskName());
+        }
+        log.info("Total : {}", stopWatch.getTotalTimeMillis());
+    }
+
+    @Test
+    void getFilesTest() {
+        executorFileSavingService.getFiles().forEach(file -> {
+            log.info("file : {}", file.getName());
+        });
+    }
+}
--- starter-test-mysql/src/test/java/com/example/service/PlaceOrderServiceTest.java
+++ starter-test-mysql/src/test/java/com/example/service/PlaceOrderServiceTest.java
@@ -2,15 +2,21 @@ package com.example.service;
 
 import com.example.aggregator.OrderAggregator;
 import com.example.executors.ReserveOrder;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mono.stacksaga.TransactionResponse;
+import org.mono.stacksaga.common.Resources;
 import org.mono.stacksaga.executor.utils.ProcessStatus;
+import org.mono.stacksaga.redis.template.BinaryBinaryRedisTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.StopWatch;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Date;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +25,10 @@ class PlaceOrderServiceTest {
 
     @Autowired
     private PlaceOrderService placeOrderService;
+
+    @Autowired
+    private BinaryBinaryRedisTemplate binaryBinaryRedisTemplate;
+
 
     @Test
     void placeOrderProcessComplete() {
@@ -34,5 +44,16 @@ class PlaceOrderServiceTest {
         stopWatch.stop();
         System.out.println("PlaceOrderProcessComplete duration : " + stopWatch.getLastTaskInfo().getTimeMillis());
         Assertions.assertEquals(ProcessStatus.PROCESS_COMPLETED, response.getFinalProcessStatus());
+    }
+
+    @Test
+    void testUploadBigfile() throws IOException {
+        byte[] channel = "test-c".getBytes();
+        byte[] bytes = FileUtils.readFileToByteArray(new File("C:\\Users\\kalha\\Downloads\\test.zip"));
+        System.out.println("length > " + bytes.length);
+        Long publish = Objects.requireNonNull(binaryBinaryRedisTemplate.getTemplate().getConnectionFactory())
+                .getConnection()
+                .publish(channel, bytes);
+        System.out.println("publish > " + publish);
     }
 }
